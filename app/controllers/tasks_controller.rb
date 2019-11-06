0859d935b8e71fb6d41e7081248bd8e15cc07f44
joanliao0525 <comeon19940525@hotmail.com>
2019-11-06 14:54:30 +0800

    create controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.user = User.first
+    if @task.save
+      redirect_to tasks_path(@task)
+    else
+      render :new
+    end
+  end
+
+
+private
+
+  def task_params
+    params.require(:task).permit(:user_id, :title, :description, :start_at, :end_at, :status, :position, :all_day, :url, :priority)
+  end
+
+  def set_task
+    @task = Task.find_by(id: params[:id])
+    redirect_to root_path if @task.nil?
+  end
+end
