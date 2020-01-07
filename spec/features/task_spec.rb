26e81fc5dda37c375a5d518e2a2cfe0d0027b24f
joanliao0525 <comeon19940525@hotmail.com>
2020-01-07 12:32:05 +0800

    rspec

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -2,5 +2,16 @@ require 'rails_helper'
 
 RSpec.feature "Task", type: :feature do
   # pending "add some scenarios (or delete) #{__FILE__}"
-  
+  context "基本功能" do
+
+    let(:user) { FactoryBot.create :user }
+    let(:task) { Task.new(user: user) }
+
+    scenario "新增任務" do
+      visit root_path
+      click_on "Create task"
+      fill_in "Title", with: "sleep"
+      click_button "Save"
+    end
+  end
 end
